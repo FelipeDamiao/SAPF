9f012c2de770f08b98c487e12e4355aae25863ab
joaoluiz15 <123167210+joaoluiz15@users.noreply.github.com>
2023-09-22 17:24:45 -0300

    22/09/2023
    Conseguir criar uma rota para uma página de login.

--- django-apps/SAPF/SAPF/settings.py
+++ django-apps/SAPF/SAPF/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6q=scbcy6=(%02=z3vyg6)zqq%jg9ylzskchwkqh12gws8^w^y'
+SECRET_KEY = 'django-insecure-8&2ajjc^y_f_sip)dmx&qxchf*&#ky@grm%8_t-!3_6c!y$jpt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'paginas',
 ]
 
 MIDDLEWARE = [
